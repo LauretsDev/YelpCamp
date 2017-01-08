58f775b9549bc724ec046cdcb842ed2d5d86f408
Diego Laurentino <diego.r.laurentino@gmail.com>
2017-01-08 18:07:48 -0200

    Continued implementing comments, styled comments and show view.

--- seeds.js
+++ seeds.js
@@ -6,17 +6,17 @@ var data = [
 	{
 		name: "Cloud's Rest", 
 		image: "https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg",
-		description: "blah blah blah"
+		description: "Bacon ipsum dolor amet turkey turducken cupim chicken swine. Short loin ham short ribs, prosciutto salami tongue bacon andouille kielbasa. Pork prosciutto tongue bacon venison, spare ribs bresaola porchetta jowl shank. Cow biltong burgdoggen turducken, drumstick cupim short ribs ham hock doner shankle filet mignon. Shoulder brisket venison, short ribs ball tip picanha drumstick kielbasa cupim salami short loin frankfurter pork chop pork belly. Ham meatloaf picanha tri-tip tongue, andouille drumstick beef. Porchetta leberkas turkey pancetta tail fatback shoulder landjaeger pork belly chicken rump biltong ball tip swine andouille."
 	},
 	{
 		name: "Desert Mesa", 
 		image: "https://farm5.staticflickr.com/4137/4812576807_8ba9255f38.jpg",
-		description: "blah blah blah"
+		description: "Bacon ipsum dolor amet turkey turducken cupim chicken swine. Short loin ham short ribs, prosciutto salami tongue bacon andouille kielbasa. Pork prosciutto tongue bacon venison, spare ribs bresaola porchetta jowl shank. Cow biltong burgdoggen turducken, drumstick cupim short ribs ham hock doner shankle filet mignon. Shoulder brisket venison, short ribs ball tip picanha drumstick kielbasa cupim salami short loin frankfurter pork chop pork belly. Ham meatloaf picanha tri-tip tongue, andouille drumstick beef. Porchetta leberkas turkey pancetta tail fatback shoulder landjaeger pork belly chicken rump biltong ball tip swine andouille."
 	},
 	{
 		name: "Canyon Floor", 
 		image: "https://farm5.staticflickr.com/4153/4835814837_feef6f969b.jpg",
-		description: "blah blah blah"
+		description: "Bacon ipsum dolor amet turkey turducken cupim chicken swine. Short loin ham short ribs, prosciutto salami tongue bacon andouille kielbasa. Pork prosciutto tongue bacon venison, spare ribs bresaola porchetta jowl shank. Cow biltong burgdoggen turducken, drumstick cupim short ribs ham hock doner shankle filet mignon. Shoulder brisket venison, short ribs ball tip picanha drumstick kielbasa cupim salami short loin frankfurter pork chop pork belly. Ham meatloaf picanha tri-tip tongue, andouille drumstick beef. Porchetta leberkas turkey pancetta tail fatback shoulder landjaeger pork belly chicken rump biltong ball tip swine andouille."
 	}
 ]
 

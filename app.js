c59373b7e8f797ad1bebd8c8ee0473e148b7aa57
Diego Laurentino <diego.r.laurentino@gmail.com>
2017-01-27 20:17:55 -0200

    Added authentication and login. Styled some views.

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ var express      = require("express"),
     app          = express(),
     bodyParser   = require("body-parser"),
     mongoose     = require("mongoose"),
+    passport     = require("passport"),
+    LocalStrategy = require("passport-local"),
     Campground   = require("./models/campground"),
     Comment      = require("./models/comment"),
+    User         = require("./models/user"),
     seedDb       = require("./seeds");
+
+var campgroundRoutes = require("./routes/campgrounds"),
+    commentRoutes    = require("./routes/comments"),
+    indexRoutes       = require("./routes/index");
 
 mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
@@ -12,94 +19,27 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 seedDb();
 
-app.get("/", function(req, res) {
-  res.render("landing");
+// PASSPORT CONFIGURATION
+app.use(require("express-session")({
+  secret: "Once again JavaScript is awesome!",
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next) {
+  res.locals.currentUser = req.user;
+  next();
 });
 
-// INDEX - show all campgrounds
-app.get("/campgrounds", function(req, res) {
-  Campground.find({}, function(err, allCampgrounds) {
-    if(err) {
-      console.log(err);
-    }
-    else {
-       res.render("campgrounds/index", {campgrounds: allCampgrounds});
-    }
-  });
-});
-
-// CREATE - add new campground to DB
-app.post('/campgrounds', function(req, res) {
-    var name = req.body.name;
-    var image = req.body.image;
-    var desc = req.body.description;
-    var newCampground = {name: name, image: image, description: desc};
-    Campground.create(newCampground, function(err, newlyCreated){
-      if(err) {
-        console.log(err);
-      }
-      else {
-        res.redirect("/campgrounds");
-      }
-    });
-});
-
-// NEW - show form to create new campground
-app.get('/campgrounds/new',function(req,res) {
-    res.render("campgrounds/new");
-});
-
-// SHOW - shows more info about one campground
-app.get("/campgrounds/:id",function(req,res) {
-    Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground) {
-      if(err) {
-        console.log(err);
-      }
-      else {
-        console.log(foundCampground);
-        res.render("campgrounds/show", {campground: foundCampground});
-      }
-    });
-});
-
-
-// =======================
-// COMMENTS ROUTES
-// =======================
-
-app.get('/campgrounds/:id/comments/new',function(req,res) {
-  Campground.findById(req.params.id, function(err, campground) {
-    if(err) {
-      console.log(err);
-    }
-    else {
-      res.render("comments/new", {campground: campground});
-    }
-  })  
-});
-
-app.post('/campgrounds/:id/comments',function(req,res) {
-    //lookup campground using ID
-    Campground.findById(req.params.id, function(err, campground) {
-      if(err) {
-        console.log(err);
-        res.redirect("/campgrounds");
-      } else {
-        Comment.create(req.body.comment, function(err, comment) {
-          if(err) {
-            console.log(err);
-          } else {
-            campground.comments.push(comment);
-            campground.save();
-            res.redirect('/campgrounds/' + campground._id);
-          }
-        })
-      }
-    })
-    //create new comment
-    //connect new comment to campground
-    //redirect campground show page
-});
+// Requiring Routes
+app.use("/", indexRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/comments", commentRoutes);
 
 app.listen(3000, function() {
   console.log("The YelpCamp Server has started!");

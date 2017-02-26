d707e07220fbeb062d6bf89fa22e59f93fe5a814
Diego Laurentino <diego.r.laurentino@gmail.com>
2017-02-25 23:33:49 -0300

    Added mongolab.

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ var campgroundRoutes = require("./routes/campgrounds"),
     commentRoutes    = require("./routes/comments"),
     indexRoutes       = require("./routes/index");
 
-mongoose.connect("mongodb://localhost/yelp_camp");
+//mongoose.connect("mongodb://localhost/yelp_camp");
+mongoose.connect("mongodb://diego:diegodiego@ds161039.mlab.com:61039/yelpcamp_diego");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
